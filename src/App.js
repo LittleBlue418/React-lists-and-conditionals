448a42c76cb91697d0806955ea1d1a108cca15f1
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-05 10:30:36 +0200

    programatically outputting lists

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
@@ -53,18 +53,12 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-              click={this.switchNameHandler.bind(this, 'h-zone')}>My Hobbies: Writing</Person>
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}>My Hobbies: Programming</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-              changed={this.nameChangedHandler}>My Hobbies: Sleeping</Person>
-          </div>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age} />
+          })}
+        </div>
       )
     }
 
