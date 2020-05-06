a101829d6244596203e6dbebf6ec49379abc3555
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-06 10:47:38 +0200

    Instructor's solve of the challenge

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import InstructorApp from './InstructorsApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InstructorApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
